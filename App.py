2a8a089e63cd1b036caa3db8713e8836e2b7d44a
Viksitsir <vmanral@hotmail.com>
2025-05-27 13:35:19 +0530

    Update App.py

--- App.py
+++ App.py
@@ -5,12 +5,12 @@ import os
 import gdown
 def download_file_from_gdrive(file_id, dest_path):
     import gdown
-    url = f"https://drive.google.com/uc?id={file_id}"
+    url = f"{file_id}"
     gdown.download(url, dest_path, quiet=False)
 
 # Use only the file IDs below (not the full share links)
-MOVIE_LIST_ID = "11gmdoQtnJC4dg_Fx8NvdP7foGRun7gLX"
-SIMILARITY_ID = "1GhlJFtSVQ-5TlTnNQ7FN-t9Bjou1xVOv"
+MOVIE_LIST_ID = "https://drive.google.com/file/d/11gmdoQtnJC4dg_Fx8NvdP7foGRun7gLX/view?usp=sharing"
+SIMILARITY_ID = "https://drive.google.com/file/d/1GhlJFtSVQ-5TlTnNQ7FN-t9Bjou1xVOv/view?usp=sharing"
 
 if not os.path.exists('movie_list.pkl'):
     download_file_from_gdrive(MOVIE_LIST_ID, 'movie_list.pkl')
